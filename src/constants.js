9f3789fb6c0cd713589384f073fe26a6a5816965
Gresham24 <tembogresham@gmail.com>
2025-01-28 23:29:07 +0200

    created color constants

---
+++ src/constants.js
@@ -0,0 +1,10 @@
+export const colors = {
+  black: "#000",
+  mattBlack: "#282828",
+  white: "#ffffff",
+  hotPink: "#FF5198",
+  palePink: "#FFDEF4",
+  blackOverlay: "rgba(16, 16, 16, 0.29)",
+  fadedWhite: "rgba(255, 255, 255, 0.81)",
+  
+};
